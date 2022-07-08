35823145d2d778e6882d8069c44774dbeb9c154e
DamiFuma02 <damianofumagalli2@gmail.com>
2022-07-09 00:55:43 +0200

    APP per Genere in funz di workType

--- WEBSERVER/app.R
+++ WEBSERVER/app.R
@@ -2,32 +2,67 @@
 # "./" == "./www/"
 # "./HTML" == "./www/HTML"
 library(shiny)
+library(dplyr)
+
+# alla compilazione vengono 
+# introdotti tutti i file presenti nella cartella R/
+
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "./STYLES/main.css")),
   p(id = "info","Fumagalli Damiano Matr: 157547   "),
+  h1(id="tit",amazon$title[1]),
+  div(id="start",
+      h1(id = "title","MOVIE ANALISYS"),
+      a(id = "startLink","START", href="./HTML/Presentazione.html"),
+      br(),
+  ),
+  
   
   div(id = "images",
-    h1(id = "title","MOVIE ANALISYS"),
-    a(id = "startLink","START", href="./HTML/Presentazione.html"),
-    br(),
+    
     img(id = "flow" ,src = "./IMAGES/FLOWCHART.png"),
     h3("WORKFLOW"),
     img(src = "./IMAGES/TIMELINE.png"),
     img(src = "./IMAGES/TIMETABLE.png"),
   ),
-  
-  
-  
 )
-server <- 
-  function(input,output){
-    re <- eventReactive(
-      input$linkID,{"ciaoooo"})
-    output$b <- renderText({ re()
-    })
+
+# APP per genere in funzione di occupation per MOVIELENS
+MLgenOccUI = fluidPage(
+  selectInput("occType","Scegli il lavoro",workTable$workType),
+  actionButton("genOcc","Mostra genere preferito"),
+  textOutput("genreOut")
+)
+
+
+genreOcc = function(occType){
+  occID = workTable[workTable$workType == occType,]$workID
+  mov = left_join(left_join(users %>% filter(Occupation == occID),ratings, by = "userID"),movies,by = "movieID")
+  prec = 0
+  gen = ""
+  for (i in 1:length(genreTable$genre)){
+    a = nrow(mov %>% filter(grepl(genreTable$genre[i],Genres)))
+    if (a > prec) {
+      prec = a
+      gen = genreTable$genre[i]
+    }
+  }
+  return(gen)
+}
+
+
+
+
+server <-  function(input,output){
+    #GENRE(WORKTYPE)
+    genOcc = eventReactive( input$genOcc, {genreOcc(input$occType)} )
+    output$genreOut = renderText({genOcc()})
   }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = MLgenOccUI, server = server)
